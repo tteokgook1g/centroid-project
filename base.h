ccac6b2ef56cf0db4b9808fca520ba2600fcdd7a
tteokgook1g <69958270+tteokgook1g@users.noreply.github.com>
2020-09-05 13:30:45 +0900

    Add files via upload

---
+++ base.h
@@ -0,0 +1,19 @@
+//This header defines basic things about this project.
+
+#pragma once
+#ifndef CENTROID_PROJECT_BASE_H_
+#define CENTROID_PROJECT_BASE_H_
+
+#ifndef NAMESPACE_DEFINE
+#define NAMESPACE_DEFINE
+#define BASE_BEGIN namespace base {
+#define BASE_END }
+#endif // !NAMESPACE_DEFINE
+
+BASE_BEGIN
+
+
+
+BASE_END
+
+#endif // !CENTROID_PROJECT_BASE_H_
